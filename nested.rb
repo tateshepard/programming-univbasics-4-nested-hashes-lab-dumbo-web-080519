891740deb939bb261cb8ddf783ce57f137cb3bca
Tate Shepard <tateshepard@yahoo.com>
2019-08-02 18:07:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,7 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -130,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
 end
